eeb528c087a5dfd1a90e9259775e579399bc1008
Charles <chuckm248@gmail.com>
2023-04-22 22:18:36 -0600

    finish my hobbies code

--- index.js
+++ index.js
@@ -1,16 +1,61 @@
 var rs = require('readline-sync');
 
-// prompt the name question
-var userName = rs.question('What is your name? ');
-// prompt fav hobbies
-var favHobby = rs.question('What are you favorite hobbies? ');
-// ask if there's more hobbies
-var anotherFavHobby = rs.question('Is there anymore hobbies that you have? ');
-// add hobbies to list
 var favHobbies = [];
-// store list of user hobbies
-favHobbies.push(favHobby);
-favHobbies.push(anotherFavHobby);
-// print user hobby list
 
-console.log('Hi, ' + userName + '. Your favorite hobbies are ' + favHobbies + '. Thank you for sharing with us.');
+// prompt the name question
+var username = rs.question('What is your name? ');
+var favHobby = rs.question(username + ', what is your favorite hobby? ');
+var moreHobbies = rs.questionInt('How many other hobbies do you have? ');
+
+favHobbies.push(favHobby);
+
+const yourHobbies = () => {
+  console.log('Hello there, ' + username + '.');
+  for (let hobby of favHobbies) {
+    console.log('One of your favorite hobbies are ' + hobby + '.');
+  }
+};
+
+const otherHobbies = (number) => {
+  for (let i = 0; i < number; i++) {
+    let nextHobby = rs.question('What is your next favorite hobby? ');
+    favHobbies.push(nextHobby);
+  }
+}
+
+const checkAnswer = () => {
+  var areYouSure = rs.keyInYN('Are you sure that\'s all you would like to do?');
+  if (areYouSure) {
+    console.log('Thank You!');
+    yourHobbies();
+  }
+  
+  if(!areYouSure) {
+    moreHobbies = rs.questionInt('How many other hobbies do you have? ');
+    counter = 0;
+    otherHobbies(moreHobbies);
+    checkAnswerAgain();
+  }
+}
+
+const checkAnswerAgain = () => {
+  var areYouSure = rs.keyInYN('Are you sure that\'s all you would like to do?');
+  if (areYouSure) {
+    console.log('Thank You!');
+    yourHobbies();
+  }
+  
+  if(!areYouSure) {
+    moreHobbies = rs.questionInt('How many other hobbies do you have? ');
+    counter = 0;
+    otherHobbies(moreHobbies);
+  }
+}
+
+
+otherHobbies(moreHobbies);
+checkAnswer();
+
+// add hobbies to list
+// store list of user hobbies
+// print user hobby list
